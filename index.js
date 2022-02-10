ddc36cb228c7a61a1f171448d3b293f2de52490f
Vernon Stephens <97752165+Blackxican93@users.noreply.github.com>
2022-02-10 17:28:26 -0600

    cors

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 //import
 const db = require("./db");
 const express = require("express");
+const cors = require("cors")
 //Port
 const PORT = process.env.PORT || 3000;
 
 const server = express();
+
+server.use(cors());
+
 server.listen(PORT, () => {
   console.log(`Server listening on PORT ${PORT}`);
 });
